f1599b1f536168edb495bfc75dc3f38cfaf0daa1
Priyanka-Kugapriya <50516526+Priyanka-Kugapriya@users.noreply.github.com>
2020-03-18 18:04:55 +0530

    Second activity - priyanka

---
+++ app/src/main/java/com/example/indigoapp/Tops_Display.java
@@ -0,0 +1,15 @@
+package com.example.indigoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tops_Display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tops_display);
+
+    }
+}
