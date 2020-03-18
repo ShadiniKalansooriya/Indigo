8a21df5b6f70d480fa5530003f1b13188ffc35a9
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-03-18 14:36:49 +0530

    after creating Register  Page - Shadini

---
+++ app/src/main/java/com/example/indigoapp/Views/RegisterDetailsActivity.java
@@ -0,0 +1,57 @@
+package com.example.indigoapp.Views;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.example.indigoapp.R;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterDetailsActivity extends AppCompatActivity {
+    TextView TextViewdate;
+    TextView TextViewdateSelect;
+
+    Calendar c;
+    DatePickerDialog dpd;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_for_register);
+
+        TextViewdate = (TextView)findViewById(R.id.textViewdate);
+        TextViewdateSelect = (TextView)findViewById(R.id.textViewDOB);
+
+        TextViewdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+
+                dpd = new DatePickerDialog(RegisterDetailsActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        TextViewdateSelect.setText(dayOfMonth + "/"+(month+1)+"/"+year);
+
+                    }
+                },day,month,year);
+                dpd.show();
+            }
+        });
+
+
+    }
+}
