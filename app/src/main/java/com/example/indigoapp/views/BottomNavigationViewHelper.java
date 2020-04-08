f378aa3e77cad4256cf34af7da3076906598e59e
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-04-09 03:51:28 +0530

    complete bottom navigation bar in my account activity

---
+++ app/src/main/java/com/example/indigoapp/views/BottomNavigationViewHelper.java
@@ -0,0 +1,8 @@
+package com.example.indigoapp.views;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+class BottomNavigationViewHelper {
+    public static void disableShiftMode(BottomNavigationView navigationView) {
+    }
+}
--- app/src/main/java/com/example/indigoapp/views/MyAccount.java
+++ app/src/main/java/com/example/indigoapp/views/MyAccount.java
@@ -1,16 +1,102 @@
 package com.example.indigoapp.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.indigoapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+import com.google.android.material.navigation.NavigationView;
 
-public class MyAccount extends AppCompatActivity {
+public class MyAccount extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+
+    BottomNavigationView navigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.nav_b_home:
+                    break;
+
+                case R.id.nav_b_shoppingbag:
+                    Intent intent = new Intent(MyAccount.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+
+                case R.id.nav_b_wishlist:
+                    Intent intent1 = new Intent(MyAccount.this, Wishlist.class);
+                    startActivity(intent1);
+                    break;
+
+                case R.id.nav_b_gallery:
+                    Intent intent2 = new Intent(MyAccount.this, GalleryView.class);
+                    startActivity(intent2);
+                    break;
+
+                case R.id.nav_b_category:
+                    Intent intent3 = new Intent(MyAccount.this, ProductsDisplay.class);
+                    startActivity(intent3);
+
+            }
+
+        return true;
+
+        }
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_for_myaccount);
+
+        /*=============Hooks================== */
+        navigationView = findViewById(R.id.bottom_navigation);
+
+        //bottom navigationview listener
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        BottomNavigationViewHelper.disableShiftMode(navigationView);
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_b_home:
+                break;
+
+            case R.id.nav_b_shoppingbag:
+                Intent intent = new Intent(MyAccount.this, MainActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_b_wishlist:
+                Intent intent1 = new Intent(MyAccount.this, Wishlist.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.nav_b_gallery:
+                Intent intent2 = new Intent(MyAccount.this, GalleryView.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.nav_b_category:
+                Intent intent3 = new Intent(MyAccount.this, ProductsDisplay.class);
+                startActivity(intent3);
+
+        }
+        return true;
     }
 }
