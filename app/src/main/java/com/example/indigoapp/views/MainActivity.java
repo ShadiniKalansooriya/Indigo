9ba29d4a162e2dde52c1af40242bb82c07360c4f
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-04-08 12:42:51 +0530

    after adding the nav Bar to main activity

--- app/src/main/java/com/example/indigoapp/views/MainActivity.java
+++ app/src/main/java/com/example/indigoapp/views/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.indigoapp.views;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.media.tv.TvContract;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.example.indigoapp.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar toolbar;
 
     Button btnlogin, btnSignin, btnGuest;
 
@@ -18,10 +31,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    /*===================Hooks======================*/
+        drawerLayout = findViewById(R.id.drawer_Layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
         btnlogin = (Button)findViewById(R.id.btnLogin);
         btnSignin = (Button)findViewById(R.id.btnSignIn);
         btnGuest = (Button)findViewById(R.id.btnGuest);
+
+        /*===========ToolBar ========= */
+
+        setSupportActionBar(toolbar);
+
+        /*===================Navigation Drawer Menu ================ */
+
+        //Hide or show items
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_Logout).setVisible(false);
+        menu.findItem(R.id.nav_MyAccount).setVisible(false);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +81,66 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_shoppingBag:
+                Intent intent =new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_WishList:
+                Intent intent1 = new Intent(MainActivity.this, Wishlist.class);
+                startActivity(intent1);
+                break;
+            case R.id.nav_MyAccount:
+                Intent intent6 =new Intent(MainActivity.this,MyAccount.class);
+                startActivity(intent6);
+                break;
+
+            case R.id.nav_Promotions:
+                Intent intent7 = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent7);
+                break;
+            case R.id.nav_Gallery:
+                Intent intent2 =new Intent(MainActivity.this,GalleryView.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.nav_about:
+                Intent intent3 = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_contactUs:
+                Intent intent4 =new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent4);
+                break;
+
+            case R.id.nav_Feedback:
+                Intent intent5 = new Intent(MainActivity.this, Feedback.class);
+                startActivity(intent5);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/indigoapp/views/MyAccount.java
@@ -0,0 +1,16 @@
+package com.example.indigoapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.indigoapp.R;
+
+public class MyAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for_myaccount);
+    }
+}
