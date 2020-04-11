5da9a35fec503790437e4f5688f2c4eb043a98fb
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-04-11 15:56:03 +0530

    Complete bottom and side nav bars to Gallery

--- app/src/main/java/com/example/indigoapp/views/Gallery.java
+++ app/src/main/java/com/example/indigoapp/views/Gallery.java
@@ -1,16 +1,161 @@
 package com.example.indigoapp.views;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.example.indigoapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Gallery extends AppCompatActivity {
+public class Gallery extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+
+    BottomNavigationView bottomNavigationView;
+    //variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar toolbar;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.nav_b_home:
+                    break;
+
+                case R.id.nav_b_shoppingbag:
+                    Intent intent = new Intent(Gallery.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+
+                case R.id.nav_b_wishlist:
+                    Intent intent1 = new Intent(Gallery.this, Wishlist.class);
+                    startActivity(intent1);
+                    break;
+
+                case R.id.nav_b_gallery:
+                    Intent intent2 = new Intent(Gallery.this, GalleryView.class);
+                    startActivity(intent2);
+                    break;
+
+                case R.id.nav_b_category:
+                    Intent intent3 = new Intent(Gallery.this, ProductsDisplay.class);
+                    startActivity(intent3);
+
+            }
+
+            return true;
+
+        }
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        /*=============Hooks================== */
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        /*===================Hooks======================*/
+        drawerLayout = findViewById(R.id.drawer_Layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setTitle("Love Indigo");
+        /*===========ToolBar ========= */
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //navigationView.setCheckedItem(R.id.nav_MyAccount);
+
+        //bottom navigationview listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_shoppingBag:
+                Intent intent =new Intent(Gallery.this,MainActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_WishList:
+                Intent intent1 = new Intent(Gallery.this, Wishlist.class);
+                startActivity(intent1);
+                break;
+            case R.id.nav_MyAccount:
+                Intent intent6 =new Intent(Gallery.this, MyAccount.class);
+                startActivity(intent6);
+                break;
+
+            case R.id.nav_Promotions:
+                Intent intent7 = new Intent(Gallery.this, MainActivity.class);
+                startActivity(intent7);
+                break;
+            case R.id.nav_Gallery:
+                Intent intent2 =new Intent(Gallery.this, GalleryView.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.nav_about:
+                Intent intent3 = new Intent(Gallery.this, MainActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_contactUs:
+                Intent intent4 =new Intent(Gallery.this,MainActivity.class);
+                startActivity(intent4);
+                break;
+
+            case R.id.nav_Feedback:
+                Intent intent5 = new Intent(Gallery.this, Feedback.class);
+                startActivity(intent5);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
