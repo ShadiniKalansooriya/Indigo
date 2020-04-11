e004b0968e95dc84c397b19ad3c4edc2ca12ef88
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-04-11 15:46:26 +0530

    GalleryView xml changes

--- app/src/main/java/com/example/indigoapp/views/GalleryView.java
+++ app/src/main/java/com/example/indigoapp/views/GalleryView.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.indigoapp.R;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GalleryView extends AppCompatActivity {
+
+    TextView textViewGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_view);
+
+        textViewGallery = findViewById(R.id.textViewUpload);
+
+        textViewGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GalleryView.this,Gallery.class);
+                startActivity(intent);
+            }
+        });
     }
 }
