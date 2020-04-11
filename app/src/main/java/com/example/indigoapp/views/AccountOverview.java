121686421fc4b310748f5fa88c8f9ea3cdbfb5bf
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-04-11 12:34:28 +0530

    after Adding Account overview

---
+++ app/src/main/java/com/example/indigoapp/views/AccountOverview.java
@@ -0,0 +1,16 @@
+package com.example.indigoapp.views;
+
+import android.os.Bundle;
+
+import com.example.indigoapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for_account_overview);
+    }
+}
--- app/src/main/java/com/example/indigoapp/views/MyAccount.java
+++ app/src/main/java/com/example/indigoapp/views/MyAccount.java
@@ -9,6 +9,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.indigoapp.R;
@@ -25,6 +26,8 @@ public class MyAccount extends AppCompatActivity implements NavigationView.OnNav
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     androidx.appcompat.widget.Toolbar toolbar;
+
+    Button btnAccountOverView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -93,6 +96,17 @@ public class MyAccount extends AppCompatActivity implements NavigationView.OnNav
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+
+        btnAccountOverView = findViewById(R.id.buttonAccountOverview);
+
+        btnAccountOverView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyAccount.this,AccountOverview.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
