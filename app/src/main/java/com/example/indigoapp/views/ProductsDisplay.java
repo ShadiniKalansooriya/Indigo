925a17b4786049c065c4f411c86fa24e81d47042
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-04-21 19:46:03 +0530

    errors fixed

--- app/src/main/java/com/example/indigoapp/views/ProductsDisplay.java
+++ app/src/main/java/com/example/indigoapp/views/ProductsDisplay.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.indigoapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ProductsDisplay extends AppCompatActivity {
 
@@ -22,7 +24,7 @@ public class ProductsDisplay extends AppCompatActivity {
         denimBtn = (Button) findViewById(R.id.denimbtn);
         dressBtn = (Button) findViewById(R.id.dressbtn);
         pantBtn = (Button) findViewById(R.id.pantbtn);
-        savingBtn = (Button) findViewById(R.id.savingbtn);
+//        savingBtn = (Button) findViewById(R.id.savingbtn);
         voucherBtn = (Button) findViewById(R.id.voucherbtn);
 
         buttonClickActivity();
